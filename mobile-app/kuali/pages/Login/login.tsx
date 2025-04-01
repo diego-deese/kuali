08db4f7f9d974a9f0565c99740f41c93d4abfbf8
Juan Pablo Escobar <a01424009@tec.mx>
2025-04-01 14:59:26 -0600

    Merge pull request #1 from diego-deese/app-dev
    added colors constants, login interface and other app configurations

--- mobile-app/kuali/pages/Login/login.tsx
+++ mobile-app/kuali/pages/Login/login.tsx
@@ -1,29 +1,41 @@
-import {  View, Text, SafeAreaView, Image, TextInput, Button } from 'react-native'
-import React from 'react'
-import styles from './login.styles'
-import colors from '../../constants/colors'
-
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  Button,
+} from "react-native"
+import React from "react"
+import styles from "./login.styles"
+import colors from "../../constants/colors"
 
 export default function LogIn() {
-    return(
-        <SafeAreaView style={styles.container}>
-            <View style={styles.imgContainer}>
-                <Image source={require('../../assets/cicataLogo.png')}/>
-            </View>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>Ingresa con tus</Text>
-                <Text style={styles.title}>credenciales</Text>
-            </View>
-            <View style={styles.inputsContainer}>
-                <Text style={styles.inputLabels}>Correo electrónico</Text>
-                <TextInput placeholder='Correo electrónico' placeholderTextColor={colors.placeholderGray} style={styles.inputText}/>
-                <Text style={styles.inputLabels}>Contraseña</Text>
-                <TextInput placeholder='***************' placeholderTextColor={colors.placeholderGray} style={styles.inputText}/>
-            </View>
-                <Button title='Iniciar sesión' color={colors.selectionBlue} />
-            <View>
-
-            </View>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imgContainer}>
+        <Image source={require("../../assets/cicataLogo.png")} />
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Ingresa con tus</Text>
+        <Text style={styles.title}>credenciales</Text>
+      </View>
+      <View style={styles.inputsContainer}>
+        <Text style={styles.inputLabels}>Correo electrónico</Text>
+        <TextInput
+          placeholder="Correo electrónico"
+          placeholderTextColor={colors.placeholderGray}
+          style={styles.inputText}
+        />
+        <Text style={styles.inputLabels}>Contraseña</Text>
+        <TextInput
+          placeholder="***************"
+          placeholderTextColor={colors.placeholderGray}
+          style={styles.inputText}
+        />
+      </View>
+      <Button title="Iniciar sesión" color={colors.selectionBlue} />
+      <View></View>
+    </SafeAreaView>
+  )
 }
