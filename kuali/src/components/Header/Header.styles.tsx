44cb15a9240e927a63257c9f688750ab8f64c01e
Juan Pablo Escobar <a01424009@tec.mx>
2025-04-17 23:03:44 -0600

    Header top comentado

--- kuali/src/components/Header/Header.styles.tsx
+++ kuali/src/components/Header/Header.styles.tsx
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 3,
     elevation: 3,
-    top: '30%',
+    //top: '30%',
   },
   tabButton: {
     marginLeft: 'auto',
