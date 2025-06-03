8c11fc4e47cd22bf8eb140f5cb996a3f5892cca1
dulcebucio <dulcenahomibucio@gmail.com>
2025-06-03 17:41:27 -0600

    vigency date field for researchers

--- kuali/src/context/UserFormContext/UserFormContext.tsx
+++ kuali/src/context/UserFormContext/UserFormContext.tsx
@@ -3,6 +3,7 @@ import { useUserForm } from './useUserForm'
 import { UserFormErrors } from '../../types/Error'
 import { Option } from '../../components/shared/SelectInput/interfaces'
 import { User } from '../../types/User'
+import { DateType } from 'react-native-ui-datepicker'
 
 interface UserFormContextProps {
   mode: 'create' | 'edit'
@@ -86,6 +87,9 @@ interface UserFormContextProps {
   placementType: Option | null
   setPlacementType: (value: Option | null) => void
   onPlacementTypeChange: (value: Option | null) => void
+
+  validity: DateType
+  onValidityDateChange: (validity: DateType) => void
 
   errors: UserFormErrors
   updateErrors: (newErrors: Partial<UserFormErrors>) => void
