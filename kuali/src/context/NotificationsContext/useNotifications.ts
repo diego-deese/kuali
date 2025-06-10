2a1c16204fa3558c52e1df35776fd00d17ee64d3
diego-deese <diego_dsgg@hotmail.com>
2025-06-09 22:29:03 -0600

    added authenticated validation when getting user notifications

--- kuali/src/context/NotificationsContext/useNotifications.ts
+++ kuali/src/context/NotificationsContext/useNotifications.ts
@@ -12,7 +12,7 @@ export const useNotifications = () => {
 
   const REFRESH_INTERVAL = 30000
 
-  const { user } = useAuth()
+  const { user, authenticated } = useAuth()
 
   const { requests } = useAppActions()
 
@@ -49,7 +49,7 @@ export const useNotifications = () => {
   }
 
   useEffect(() => {
-    if (requests.isSendingRequest) return
+    if (requests.isSendingRequest || !authenticated) return
 
     if (user?.role.role_id !== Roles.ADMIN) {
       setNotifications([])
@@ -61,7 +61,7 @@ export const useNotifications = () => {
   
       return () => clearInterval(interval)
     }
-  }, [])
+  }, [authenticated, requests.isSendingRequest])
 
   return {
     notifications: notifications ?? [],
